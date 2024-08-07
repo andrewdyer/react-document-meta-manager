acd52d9e3c2595ab7da121004a327a932346d90d
andrewdyer <andrewdyer@outlook.com>
2024-08-07 22:47:37 +0100

    refactor: :hammer: update exports
    Added missing context exports

---
+++ src/contexts/index.ts
@@ -0,0 +1,7 @@
+// DocumentTitleContext
+export * from './DocumentTitleContext';
+export { default as DocumentTitleContext } from './DocumentTitleContext';
+
+// DocumentTitleProvider
+export * from './DocumentTitleProvider';
+export { default as DocumentTitleProvider } from './DocumentTitleProvider';
